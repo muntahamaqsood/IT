e14b68e537d23cbf4735911152c7d5b68e5c411f
muntahamaqsood <80307706+muntahamaqsood@users.noreply.github.com>
2021-03-31 16:36:50 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace New_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x*x+3*x-4");
+            Console.WriteLine("(x+y)*x");
+            Console.WriteLine("(x+3*y)/(2*x-y)");
+            Console.WriteLine("1/(x*x+x+3)");
+            Console.WriteLine("(x+y)/7");
+            Console.WriteLine("2*b*c*c*c");
+            Console.WriteLine("x=(3*y)/(5-z)");
+            Console.WriteLine("(x+32/(y-32)-(x-2*y)");
+            Console.WriteLine("z=area*sqrt(area)");
+            Console.WriteLine("res=(3*i*j*k+pow9k,9))/(7*i*j*k-5*sqrt(j+k))");
+            
+        }
+    }
+}

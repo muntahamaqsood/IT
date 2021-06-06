cd2fdb9534a58d31d5fb6e5edf2abbca55e62d35
muntahamaqsood <80307706+muntahamaqsood@users.noreply.github.com>
2021-06-06 18:45:26 +0500

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+            var fullName = firstName + " " + lastName;
+            
+            var myFullName = string.Format("My name is {0} {1}",firstName, lastName);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+
+
+        }
+    }
+}
+

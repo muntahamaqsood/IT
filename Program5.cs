a539aac03a560546038647f260007798b382ef3f
muntahamaqsood <80307706+muntahamaqsood@users.noreply.github.com>
2021-06-06 19:04:43 +0500

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var firstName = "Mosh";
+            var lastName = "Hamedani";
+            var fullName = firstName + " " + lastName;
+            
+            var myFullName = string.Format("My name is {0} {1}",firstName, lastName);
+            var names = new string[3] { "John", "Jack", "Mary" };
+            var formattedNames = string.Join(",", names);
+            var text = "Hi John\nLook into the following paths\nc:\\folder1\\folder2\nc:\\folder3\\folder4";
+            Console.WriteLine(text);
+
+
+
+        }
+    }
+}
+

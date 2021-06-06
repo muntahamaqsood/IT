b3b4d896b4aad08cad3f500d3b32071bc1bf6f05
muntahamaqsood <80307706+muntahamaqsood@users.noreply.github.com>
2021-06-06 19:24:20 +0500

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+            var methodId = 3 ;
+            Console.WriteLine((ShippingMethod)methodId);
+            Console.WriteLine(method.ToString());
+
+        }
+    }
+}
+
